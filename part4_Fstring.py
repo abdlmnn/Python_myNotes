819d446b2afcdb14e45b35ca008d3254f9f30094
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2024-09-23 23:19:36 +0800

    part 4 added

---
+++ part4_Fstring.py
@@ -0,0 +1,10 @@
+math = input('Score in math? ')
+programming = 95
+# i created two variable math and programming subjects
+# they have value each of them
+
+score = f'My score in math is {math} and programming is {programming}.'
+# i create another variable called score so i can print the variable score
+# you notice the f before the single quotation mark and it has curly bracket
+# the { } you can assign a variable name that you created
+print(score)
