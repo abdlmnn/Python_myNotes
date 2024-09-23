458ade751372d34b251992d37b800dc2e1f0ef79
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2024-09-23 23:25:38 +0800

    part 4 added

--- part4_Fstring.py
+++ part4_Fstring.py
@@ -1,10 +1,12 @@
 math = input('Score in math? ')
 programming = 95
 # i created two variable math and programming subjects
-# they have value each of them
+# one is input the other one had already a value
 
 score = f'My score in math is {math} and programming is {programming}.'
 # i create another variable called score so i can print the variable score
 # you notice the f before the single quotation mark and it has curly bracket
 # the { } you can assign a variable name that you created
+
+# you are telling this formatted string to subsitute the value of a variable
 print(score)
